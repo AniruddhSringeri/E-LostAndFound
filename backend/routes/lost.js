f812c68bff72d9ae889357830090567d94e9de3f
Praneeth <bpraneeth2000@gmail.com>
2020-12-04 20:05:50 +0530

    server+backend etc

---
+++ backend/routes/lost.js
@@ -0,0 +1,48 @@
+import express from 'express'
+const router = express.Router()
+import auth from '../middleware/auth.js' 
+import Lost from '../models/Lost.js'
+
+router.post('/', (req, res) => {
+    const dbItem = req.body
+    const {user_id, item, place_lost } = req.body
+    if(!user_id || !item.name || !item.category || !item.description || !item.img || !place_lost){
+        return res.status(400).json({msg:'Please enter all the fields'})
+    }
+
+    Lost.create(dbItem, (err, data) => {
+        if(err){
+            res.status(500).send(err)
+        }
+        else{
+            res.status(201).send(data)
+        }
+    })
+})
+
+router.get('/', (req, res) => {
+    
+
+    Lost.find((err, data) => {
+        if(err){
+            res.status(500).send(err)
+        }
+        else{
+            // const items = data.map(x => x.item)
+            res.status(200).send(data)
+        }
+
+    })
+})
+
+router.get('/:id', (req, res) => {
+    Lost.find({_id: req.params.id}, (err, data) => {
+        if(err){
+            res.status(500).send(err)
+        }
+        else{
+            res.status(200).send(data[0])
+        }
+    })
+})
+export default router
---
+++ frontend/src/SignupForm.js
@@ -0,0 +1,78 @@
+import React, { useState, useEffect } from "react";
+import { Button, Form, FormGroup, Label, Input, FormText, Alert } from 'reactstrap';
+import {useHistory} from "react-router-dom";
+import axios from "./axios";
+import { register } from './actions/authActions'
+
+import { connect } from 'react-redux'
+
+function SignupForm(props){
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [msg, setMsg] = useState(null)
+    const history = useHistory()
+
+    useEffect(()=>{
+        const { error } = props
+        if(error.id === 'REGISTER_FAIL'){
+            setMsg(error.msg)
+        }else {
+            setMsg(null)
+        }
+    })
+
+    function handleChange(event){
+        const {name, value} = event.target
+        if(name == "name") setName(value)
+        else if(name == "email") setEmail(value)
+        else setPassword(value)
+    }
+
+    function handleSubmit(event){
+        event.preventDefault()
+        
+        // Create user object
+        const newUser = {
+            name,
+            email,
+            password
+        }
+        
+        props.register(newUser)
+        
+        if(props.isAuthenticated){
+            history.push('/lost')
+        }
+    }
+    return(
+        <div style = {{width:"30rem", border: "solid", borderWidth:"1px", borderRadius:"2%"}} >
+            { msg? <Alert color="danger">{ msg }</Alert>: null}
+            <Form style = {{width:"25rem", padding:"1rem"}}>
+                <h2>Sign up</h2>
+                <FormGroup>
+                    <Label>Name</Label>
+                    <Input name = "name" onChange = {handleChange} value = {name} placeholder = "Name"/>
+                </FormGroup>
+                <FormGroup style = {{padding:"1rem",width:"20rem"}}>
+                    <Label for="exampleEmail">Email</Label>
+                    <Input type="email" name="email" onChange = {handleChange} value = {email} id="exampleEmail" placeholder="enter e-mail" />
+                </FormGroup>
+                <FormGroup style = {{padding:"1rem", width:"20rem"}}>
+                    <Label for="examplePassword">Password</Label>
+                    <Input type="password" name="password" onChange = {handleChange} value = {password} id="examplePassword" placeholder="password placeholder" />
+                </FormGroup>
+                <Button color="success" onClick = {handleSubmit}>Sign up</Button>
+                
+
+            </Form>
+            
+        </div>
+    )
+}
+const mapStateToProps = state => ({
+    isAuthenticated: state.auth.isAuthenticated,
+    error: state.error
+})
+
+export default connect(mapStateToProps,{ register })(SignupForm)
